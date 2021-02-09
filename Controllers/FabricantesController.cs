9ca13fa91dfefff323c546a701ed8d08ccb1363b
Matheus Henrique Gadelha da Costa <gadelha.matheus@academico.ifrn.edu.br>
2021-02-09 14:07:02 +0000

    Alteração - Erro bugadao ai

---
+++ Controllers/FabricantesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Context;
+
+namespace WebApplication1.Controllers
+{
+    class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Fabricante
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(c => c.Nome));
+        }
+
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        // POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fabricante fabricante)
+        {
+            context.Fabricantes.Add(fabricante);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
