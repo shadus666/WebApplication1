88819f92f897a6611226c5fd3e3a306bd15c7ea5
Matheus Henrique Gadelha da Costa <gadelha.matheus@academico.ifrn.edu.br>
2021-02-14 17:56:12 +0000

    Só progresso

--- Controllers/FabricantesController.cs
+++ Controllers/FabricantesController.cs
@@ -101,6 +101,7 @@ namespace WebApplication1.Controllers
             Fabricante fabricante = context.Fabricantes.Find(id);
             context.Fabricantes.Remove(fabricante);
             context.SaveChanges();
+            TempData["Message"] = "Fabricante " + fabricante.Nome.ToUpper() + " foi removido";
             return RedirectToAction("Index");
         }
     }
